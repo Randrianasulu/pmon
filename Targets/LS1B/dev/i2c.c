f16bf59a721db9ce80a609e499712df6ba8d8843
qiaochong <qiaochong@loongson.cn>
2015-08-11 10:01:01 +0800

    fix ls1a, ls1b i2c function.
    Signed-off-by: qiaochong <qiaochong@loongson.cn>

--- Targets/LS1B/dev/i2c.c
+++ Targets/LS1B/dev/i2c.c
@@ -17,37 +17,38 @@ void i2c_stop()
 
 unsigned char i2c_rec_s(unsigned char *addr,int addrlen,unsigned char* buf ,int count)
 {
-int i;
-int j;
+	int i;
+	int j;
 	unsigned char value;
+	unsigned char reg = addr[addrlen-1];
 	for(i=0;i<count;i++)
 	{
-again:
 		for(j=0;j<addrlen;j++)
 		{
-		/*write slave_addr*/
-  * GS_SOC_I2C_TXR = addr[j];
-  * GS_SOC_I2C_CR  = (j == 0)? (CR_START|CR_WRITE):CR_WRITE; /* start on first addr */
-   while(*GS_SOC_I2C_SR & SR_TIP);
+			/*write slave_addr*/
+			* GS_SOC_I2C_TXR = (j==addrlen-1)?reg:addr[j];
+			* GS_SOC_I2C_CR  = (j == 0)? (CR_START|CR_WRITE):CR_WRITE; /* start on first addr */
+			while(*GS_SOC_I2C_SR & SR_TIP);
 
-  if((* GS_SOC_I2C_SR) & SR_NOACK) { printf("read no ack %d\n",__LINE__); i2c_stop();goto again;}
+			if((* GS_SOC_I2C_SR) & SR_NOACK) { printf("read no ack %d\n",__LINE__); i2c_stop();return i;}
 		}
 
 		/*write slave_addr*/
-  * GS_SOC_I2C_TXR = addr[0]|1;
-  * GS_SOC_I2C_CR  = CR_START|CR_WRITE; /* start on first addr */
-   while(*GS_SOC_I2C_SR & SR_TIP);
+		* GS_SOC_I2C_TXR = addr[0]|1;
+		* GS_SOC_I2C_CR  = CR_START|CR_WRITE; /* start on first addr */
+		while(*GS_SOC_I2C_SR & SR_TIP);
 
-  if((* GS_SOC_I2C_SR) & SR_NOACK) { printf("read no ack %d\n",__LINE__); i2c_stop();goto again;}
+		if((* GS_SOC_I2C_SR) & SR_NOACK) { printf("read no ack %d\n",__LINE__); i2c_stop();return i;}
 
-  * GS_SOC_I2C_CR  = CR_READ|I2C_WACK; /*last read not send ack*/
-  while(*GS_SOC_I2C_SR & SR_TIP);
+		* GS_SOC_I2C_CR  = CR_READ|I2C_WACK; /*last read not send ack*/
+		while(*GS_SOC_I2C_SR & SR_TIP);
 
-    buf[i] = * GS_SOC_I2C_TXR;
-  * GS_SOC_I2C_CR  = CR_STOP;
-  * GS_SOC_I2C_SR;
-  while(*GS_SOC_I2C_SR & SR_BUSY);
- 
+		buf[i] = * GS_SOC_I2C_TXR;
+		* GS_SOC_I2C_CR  = CR_STOP;
+		* GS_SOC_I2C_SR;
+		while(*GS_SOC_I2C_SR & SR_BUSY);
+		reg++;
+
 	}
 
 	return count;
@@ -55,93 +56,95 @@ again:
 
 unsigned char i2c_send_s(unsigned char *addr,int addrlen,unsigned char * buf ,int count)
 {
-int i;
-int j;
+	int i;
+	int j;
+	unsigned char reg = addr[addrlen-1];
 	for(i=0;i<count;i++)
 	{
-again:
 		for(j=0;j<addrlen;j++)
 		{
-		/*write slave_addr*/
-  * GS_SOC_I2C_TXR = addr[j];
-  * GS_SOC_I2C_CR  = j == 0? (CR_START|CR_WRITE):CR_WRITE; /* start on first addr */
-   while(*GS_SOC_I2C_SR & SR_TIP);
+			/*write slave_addr*/
+			* GS_SOC_I2C_TXR = (j==addrlen-1)?reg:addr[j];
+			* GS_SOC_I2C_CR  = j == 0? (CR_START|CR_WRITE):CR_WRITE; /* start on first addr */
+			while(*GS_SOC_I2C_SR & SR_TIP);
 
-  if((* GS_SOC_I2C_SR) & SR_NOACK) { printf("write no ack %d\n",__LINE__); i2c_stop();goto again;}
+			if((* GS_SOC_I2C_SR) & SR_NOACK) { printf("write no ack %d\n",__LINE__); i2c_stop();return i;}
 		}
 
 
-  * GS_SOC_I2C_TXR = buf[i];
-  * GS_SOC_I2C_CR = CR_WRITE|CR_STOP;
-  while(*GS_SOC_I2C_SR & SR_TIP);
+		* GS_SOC_I2C_TXR = buf[i];
+		* GS_SOC_I2C_CR = CR_WRITE|CR_STOP;
+		while(*GS_SOC_I2C_SR & SR_TIP);
 
-  if((* GS_SOC_I2C_SR) & SR_NOACK) { printf("write no ack %d\n",__LINE__); i2c_stop();goto again;}
+		if((* GS_SOC_I2C_SR) & SR_NOACK) { printf("write no ack %d\n",__LINE__); i2c_stop();return i;}
+		reg++;
 	}
-  while(*GS_SOC_I2C_SR & SR_BUSY);
+	while(*GS_SOC_I2C_SR & SR_BUSY);
 	return count;
 }
 
 
 unsigned char i2c_rec_b(unsigned char *addr,int addrlen,unsigned char* buf ,int count)
 {
-int i;
-int j;
+	int i;
+	int j;
 
-	
+
 	unsigned char value;
 
-		for(j=0;j<addrlen;j++)
-		{
+	for(j=0;j<addrlen;j++)
+	{
 		/*write slave_addr*/
-  * GS_SOC_I2C_TXR = addr[j];
-  * GS_SOC_I2C_CR  = j == 0? (CR_START|CR_WRITE):CR_WRITE; /* start on first addr */
-   while(*GS_SOC_I2C_SR & SR_TIP);
+		* GS_SOC_I2C_TXR = addr[j];
+		* GS_SOC_I2C_CR  = j == 0? (CR_START|CR_WRITE):CR_WRITE; /* start on first addr */
+		while(*GS_SOC_I2C_SR & SR_TIP);
 
-  if((* GS_SOC_I2C_SR) & SR_NOACK) return i;
-		}
+		if((* GS_SOC_I2C_SR) & SR_NOACK) return i;
+	}
 
 
-  * GS_SOC_I2C_TXR = addr[0]|1;
-  * GS_SOC_I2C_CR  = CR_START|CR_WRITE;
-  if((* GS_SOC_I2C_SR) & SR_NOACK) return i;
+	* GS_SOC_I2C_TXR = addr[0]|1;
+	* GS_SOC_I2C_CR  = CR_START|CR_WRITE;
+	if((* GS_SOC_I2C_SR) & SR_NOACK) return i;
 
-		for(i=0;i<count;i++)
-		{
-  * GS_SOC_I2C_CR  = CR_READ;
-  while(*GS_SOC_I2C_SR & SR_TIP);
+	for(i=0;i<count;i++)
+	{
+		* GS_SOC_I2C_CR  = (i==count-1)?CR_READ|I2C_WACK:CR_READ; /*last read not send ack*/
+		while(*GS_SOC_I2C_SR & SR_TIP);
 
-    buf[i] = * GS_SOC_I2C_TXR;
-		}
-  * GS_SOC_I2C_CR  = CR_STOP;
+		buf[i] = * GS_SOC_I2C_TXR;
+	}
+	* GS_SOC_I2C_CR  = CR_STOP;
 
 	return count;
 }
+
 unsigned char i2c_send_b(unsigned char *addr,int addrlen,unsigned char * buf ,int count)
 {
-int i;
-int j;
-		for(j=0;j<addrlen;j++)
-		{
+	int i;
+	int j;
+	for(j=0;j<addrlen;j++)
+	{
 		/*write slave_addr*/
-  * GS_SOC_I2C_TXR = addr[j];
-  * GS_SOC_I2C_CR  = j == 0? (CR_START|CR_WRITE):CR_WRITE; /* start on first addr */
-   while(*GS_SOC_I2C_SR & SR_TIP);
+		* GS_SOC_I2C_TXR = addr[j];
+		* GS_SOC_I2C_CR  = j == 0? (CR_START|CR_WRITE):CR_WRITE; /* start on first addr */
+		while(*GS_SOC_I2C_SR & SR_TIP);
 
-  if((* GS_SOC_I2C_SR) & SR_NOACK) return i;
-		}
+		if((* GS_SOC_I2C_SR) & SR_NOACK) return i;
+	}
 
 
 	for(i=0;i<count;i++)
 	{	
-  * GS_SOC_I2C_TXR = buf[i];
-  * GS_SOC_I2C_CR = CR_WRITE;
-  while(*GS_SOC_I2C_SR & SR_TIP);
+		* GS_SOC_I2C_TXR = buf[i];
+		* GS_SOC_I2C_CR = CR_WRITE;
+		while(*GS_SOC_I2C_SR & SR_TIP);
 
-  if((* GS_SOC_I2C_SR) & SR_NOACK) return i;
+		if((* GS_SOC_I2C_SR) & SR_NOACK) return i;
 
 	}
-  * GS_SOC_I2C_CR  = CR_STOP;
-  while(*GS_SOC_I2C_SR & SR_BUSY);
+	* GS_SOC_I2C_CR  = CR_STOP;
+	while(*GS_SOC_I2C_SR & SR_BUSY);
 	return count;
 }
 
