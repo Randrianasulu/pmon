7045e54392b3648b6e2d31aff1c9591adb5a6f8d
QiaoChong <qiaochong@loongson.cn>
2018-02-02 20:08:09 +0800

    increase nand max pagesize and max oobsize.
    Change-Id: Ided25f602f02411c0df7898470ba33f3fc992e9f
    Signed-off-by: QiaoChong <qiaochong@loongson.cn>

--- include/linux/mtd/nand.h
+++ include/linux/mtd/nand.h
@@ -37,8 +37,8 @@ extern void nand_release (struct mtd_info *mtd);
  * is supported now. If you add a chip with bigger oobsize/page
  * adjust this accordingly.
  */
-#define NAND_MAX_OOBSIZE	256
-#define NAND_MAX_PAGESIZE	4096
+#define NAND_MAX_OOBSIZE	0x1000
+#define NAND_MAX_PAGESIZE	0x10000
 
 /*
  * Constants for hardware specific CLE/ALE/NCE function
